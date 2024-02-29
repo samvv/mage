cc66afdf0a4853c292335acbfb19003f828c9b3c
Sam Vervaeck <samvv@pm.me>
2024-02-29 21:08:44 +0100

    Add a class that can generate temporary names to util.py

--- src/magelang/util.py
+++ src/magelang/util.py
@@ -15,3 +15,20 @@ def nonnull(value: T | None) -> T:
     assert(value is not None)
     return value
 
+class NameGenerator:
+
+    def __init__(self, default_prefix = 'tmp_') -> None:
+        self._mapping: dict[str, int] = {}
+        self._default_prefix = default_prefix
+
+    def __call__(self, prefix: str | None = None) -> str:
+        if prefix is None:
+            prefix = self._default_prefix
+        count = self._mapping.get(prefix, 0)
+        name = prefix + str(count)
+        self._mapping[prefix] = count + 1
+        return name
+
+    def reset(self) -> None:
+        self._mapping = {}
+
