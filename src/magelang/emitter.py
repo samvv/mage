880f291a3f6e1c702930ce1f254c6b106dc65f2c
Sam Vervaeck <samvv@pm.me>
2024-03-02 21:20:29 +0100

    Create simple hand-written emitter for the Mage language

---
+++ src/magelang/emitter.py
@@ -0,0 +1,148 @@
+
+from io import StringIO
+from sweetener import IndentWriter
+
+from .ast import *
+
+def emit(node: Node) -> str:
+
+    string = StringIO()
+    out = IndentWriter(string)
+
+    def escape(ch: str) -> str:
+        if ch.isprintable():
+            return ch
+        code = ord(ch)
+        if code <= 0x7F:
+            return f'\\x{code:02X}'
+        return f'\\u{code:04x}'
+
+    def is_wide(expr: Expr) -> bool:
+        if isinstance(expr, SeqExpr):
+            return len(expr.elements) > 1 or any(is_wide(element) for element in expr.elements)
+        return False
+
+    def visit(node: Node) -> None:
+
+        if isinstance(node, Grammar):
+            for rule in node.rules:
+                visit(rule)
+            return
+
+        if isinstance(node, Rule):
+            if node.is_public:
+                out.write('pub ')
+            if node.is_extern:
+                out.write('extern ')
+            if node.is_token:
+                out.write('token ')
+            out.write(node.name)
+            if node.type_name is not None:
+                out.write(' -> ')
+                out.write(node.type_name)
+            if node.expr is not None:
+                out.write(' = ')
+                visit(node.expr)
+            out.write('\n')
+            return
+
+        if isinstance(node, RefExpr):
+            out.write(node.name)
+            return
+
+        if isinstance(node, CharSetExpr):
+            out.write('[')
+            for element in node.elements:
+                if isinstance(element, str):
+                    out.write(escape(element))
+                else:
+                    low, high = element
+                    out.write(escape(low))
+                    out.write('-')
+                    out.write(escape(high))
+            out.write(']')
+            return
+
+        if isinstance(node, LitExpr):
+            out.write(repr(node.text))
+            return
+
+        if isinstance(node, SeqExpr):
+            first = True
+            for element in node.elements:
+                if first: first = False
+                else: out.write(' ')
+                visit(element)
+            return
+
+        if isinstance(node, ChoiceExpr):
+            first = True
+            for element in node.elements:
+                if first: first = False
+                else: out.write(' | ')
+                visit(element)
+            return
+
+        if isinstance(node, ListExpr):
+            out.write('(')
+            visit(node.element)
+            out.write(' % ')
+            visit(node.separator)
+            out.write(')')
+            return
+
+        if isinstance(node, HideExpr):
+            out.write('\\')
+            wide = is_wide(node)
+            if wide:
+                out.write('(')
+            visit(node.expr)
+            if wide:
+                out.write(')')
+            return
+
+        if isinstance(node, LookaheadExpr):
+            out.write('!' if node.is_negated else '&')
+            wide = is_wide(node)
+            if wide:
+                out.write('(')
+            visit(node.expr)
+            if wide:
+                out.write(')')
+            return
+
+        if isinstance(node, RepeatExpr):
+            if node.min == 0 and node.max == 1:
+                wide = is_wide(node.expr)
+                if wide:
+                    out.write('(')
+                visit(node.expr)
+                if wide:
+                    out.write(')')
+                out.write('?')
+            elif node.min == 0 and node.max == POSINF:
+                wide = is_wide(node.expr)
+                if wide:
+                    out.write('(')
+                visit(node.expr)
+                if wide:
+                    out.write(')')
+                out.write('*')
+            elif node.min == 1 and node.max == POSINF:
+                wide = is_wide(node.expr)
+                if wide:
+                    out.write('(')
+                visit(node.expr)
+                if wide:
+                    out.write(')')
+                out.write('+')
+            else:
+                raise NotImplementedError()
+            return
+
+        raise RuntimeError(f'unexepected {node}')
+
+    visit(node)
+
+    return string.getvalue()
+
