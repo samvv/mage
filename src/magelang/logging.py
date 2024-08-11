4c46b5b6944b2225c7b32e6b5ebeac7ba79b2e0a
Sam Vervaeck <samvv@pm.me>
2024-08-12 01:53:49 +0200

    Add missing logging.py file

---
+++ src/magelang/logging.py
@@ -0,0 +1,15 @@
+
+import sys
+
+def info(message: str) -> None:
+    sys.stderr.write(f'Info: {message}\n')
+
+def warn(message: str) -> None:
+    sys.stderr.write(f'Warning: {message}\n')
+
+def error(message: str) -> None:
+    sys.stderr.write(f'Error: {message}\n')
+
+def fatal(message: str) -> None:
+    sys.stderr.write(f'Fatal: {message}\n')
+
