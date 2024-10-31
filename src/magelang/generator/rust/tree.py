34858018d5eb4dbd8350d3fe4175c1bfc056e6a7
Sam Vervaeck <samvv@pm.me>
2024-10-31 17:40:40 +0100

    Add some more logic to Rust treegen

--- src/magelang/generator/rust/tree.py
+++ src/magelang/generator/rust/tree.py
@@ -1,6 +1,22 @@
 
+from collections.abc import Generator, Iterable
+from typing import TypeVar
 from magelang.lang.rust.cst import *
 from magelang.treespec import Specs, TokenSpec
+from magelang.util import to_camel_case, unreachable
+
+def to_type_name(name: str) -> str:
+    return to_camel_case(name)
+
+def make_rust_ident(name: str) -> RustPath:
+    return RustPath([ RustPathSegment(name) ])
+
+T = TypeVar('T')
+
+def external_type_to_rust_type_expr(name: str) -> RustTypeExpr:
+    if name == 'String':
+        return RustPathTypeExpr(make_rust_ident('String'))
+    unreachable()
 
 def generate_tree(
     specs: Specs,
@@ -9,8 +25,43 @@ def generate_tree(
 
     items = []
 
+    def list_comma(elements: Iterable[T]) -> list[T | RustComma]:
+        it = iter(elements)
+        out = []
+        try:
+            out.append(next(it))
+        except StopIteration:
+            return []
+        while True:
+            try:
+                el = next(it)
+            except StopIteration:
+                break
+            out.append(RustComma())
+            out.append(el)
+        return out
+
+    def make_derive(*names) -> RustMeta:
+        return RustMetaParenthesized(
+            path=make_rust_ident('derive'),
+            tokens=list_comma(RustIdent(name) for name in names),
+        )
+
     for spec in specs:
         if isinstance(spec, TokenSpec):
-            items.append(RustStructItem(pub_keyword=RustPubKeyword(), name=spec.name, fields=[]))
+            fields = []
+            if not spec.is_static:
+                fields.append(
+                    RustField(
+                        name='value',
+                        type_expr=external_type_to_rust_type_expr(spec.field_type),
+                    )
+                )
+            items.append(RustStructItem(
+                attrs=[ make_derive('Clone', 'Debug') ],
+                pub_keyword=RustPubKeyword(),
+                name=to_type_name(spec.name),
+                fields=fields
+            ))
 
     return RustSourceFile(items=items)
