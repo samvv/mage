32c81d3aba1494f85ccee28b3ba4d720bba1c08c
Sam Vervaeck <samvv@pm.me>
2024-11-18 20:03:49 +0100

    x.py: Make build script use internal argument parser library

--- x.py
+++ x.py
@@ -7,7 +7,6 @@ from pathlib import Path
 from typing import TypeVar
 
 project_root = Path(__file__).parent.resolve()
-lang_dir = project_root / 'src' / 'magelang' / 'lang'
 
 _T = TypeVar('_T')
 _R = TypeVar('_R')
@@ -19,33 +18,51 @@ def _map_keys(mapping: dict[_T, _V], proc: Callable[[_T], _R]) -> dict[_R, _V]:
         out[proc(key)] = value
     return out
 
-def main() -> int:
+def _generate_internal(name: str, force: bool) -> int:
 
     import magelang
 
     files = magelang.generate_files(
-        'grammars/python.mage',
+        project_root / 'grammars' / (name + '.mage'),
         'python',
         prefix='py',
         enable_cst=True,
-        enable_ast=True,
+        # enable_ast=True,
     )
 
-    files = _map_keys(files, lambda fname: str((lang_dir / 'python' / fname).resolve()))
+    files = _map_keys(files, lambda fname: f'src/magelang/lang/{name}/{fname}')
 
-    output_modified = False
-    result = subprocess.run('git status --porcelain=v1', stdout=subprocess.PIPE, check=True, shell=True)
-    for line in result.stdout.decode('utf-8').splitlines():
-        state = line[:2].strip()
-        path = str((project_root / line[3:]).resolve())
-        if path in files:
-            del files[path]
-            print(f"Not generating {path} because it has local changes on disk")
-            output_modified = True
+    if not force:
 
+        # Filter out those files that would lose changes when they are actually written.
+
+        result = subprocess.run('git status --porcelain=v1', stdout=subprocess.PIPE, check=True, shell=True)
+        for line in result.stdout.decode('utf-8').splitlines():
+            state = line[:2].strip()
+            path = line[3:]
+            if path in files:
+                del files[path]
+                print(f"Not generating {path} because it has uncommitted changes on disk")
+
+    # Write the files with force because even though Git might report the file
+    # as unchanged, it still takes up space on disk.
     magelang.write_files(files, dest_dir=project_root, force=True)
 
     return 0
 
+def generate(next: bool = False, force: bool = False) -> int:
+    print(force)
+    src_dir = project_root / ('src' if next else 'lkg')
+    sys.path.insert(0, str(src_dir))
+    _generate_internal('mage', force=force)
+    # _generate_checked('treespec', force=force)
+    _generate_internal('python', force=force)
+    return 0
+
+def test() -> int:
+    import pytest
+    return pytest.main([ 'src/magelang' ])
+
 if __name__ == '__main__':
-    sys.exit(main())
+    from magelang.cli import run
+    sys.exit(run(__name__))
