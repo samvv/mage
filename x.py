5ab2c41f752715c25076a20a81df373cf4eaeb38
Sam Vervaeck <samvv@pm.me>
2024-11-20 18:52:28 +0100

    x.py: Split `bump` into multiple sub-tasks

--- x.py
+++ x.py
@@ -1,66 +1,58 @@
 #!/usr/bin/env python3
 
-from collections.abc import Callable
+from functools import lru_cache
 import sys
 import subprocess
 from pathlib import Path
-from typing import Any, Iterable, TypeVar
+from typing import Any, Literal
 import shutil
 from packaging.version import Version, parse as parse_version
 import git
 
 project_root = Path(__file__).parent.resolve()
 
-_T = TypeVar('_T')
-_R = TypeVar('_R')
-_V = TypeVar('_V')
+repo = git.Repo(project_root)
 
-def _map_keys(mapping: dict[_T, _V], proc: Callable[[_T], _R]) -> dict[_R, _V]:
-    out = {}
-    for key, value in mapping.items():
-        out[proc(key)] = value
-    return out
-
-def _generate_internal(name: str, force: bool) -> int:
+def _generate_internal(name: str, prefix: str, force: bool) -> int:
 
     import magelang
 
     files = magelang.generate_files(
         project_root / 'grammars' / (name + '.mage'),
         'python',
+        prefix=prefix,
         enable_cst=True,
         # enable_ast=True,
     )
 
-    files = _map_keys(files, lambda fname: f'src/magelang/lang/{name}/{fname}')
+    out_dir = project_root / 'src' / 'magelang' / 'lang' / name
 
     if not force:
 
         # Filter out those files that would lose changes when they are actually written.
 
-        result = subprocess.run('git status --porcelain=v1', stdout=subprocess.PIPE, check=True, shell=True)
-        for line in result.stdout.decode('utf-8').splitlines():
-            state = line[:2].strip()
-            path = line[3:]
-            if path in files:
-                del files[path]
-                print(f"Not generating {path} because it has uncommitted changes on disk")
+        for path in _get_dirty():
+            if _path_same_or_part_of(out_dir, path):
+                rel_path = str(path.absolute().relative_to(out_dir))
+                if rel_path in files:
+                    del files[rel_path]
+                    print(f"Not generating {path} because it has uncommitted changes on disk")
 
     # Write the files with force because even though Git might report the file
     # as unchanged, it still takes up space on disk.
-    magelang.write_files(files, dest_dir=project_root, force=True)
+    magelang.write_files(files, dest_dir=out_dir, force=True)
 
     return 0
 
 def generate(next: bool = False, force: bool = False) -> int:
-    print(force)
-    src_dir = project_root / ('src' if next else 'lkg')
+    src_dir = project_root / ('src' if next else 'pkg/stable')
     sys.path.insert(0, str(src_dir))
-    _generate_internal('mage', force=force)
+    _generate_internal('mage', 'mage', force=force)
     # _generate_checked('treespec', force=force)
-    _generate_internal('python', force=force)
+    _generate_internal('python', 'py', force=force)
     return 0
 
+@lru_cache
 def _get_pyproject_toml() -> dict[str, Any]:
     import toml
     return toml.load(project_root / 'pyproject.toml')
@@ -75,28 +67,24 @@ def _build_version(major: int, minor: int, micro: int, dev: int | None) -> str:
         out += f'.dev{dev}'
     return out
 
-def _path_part_of(root: Path, child: Path) -> bool:
-    return root.absolute() in child.absolute().parents
+def _path_same_or_part_of(root: Path, child: Path) -> bool:
+    return root == child or root.absolute() in child.absolute().parents
 
-def _git_list_files_in_dir(repo: git.Repo, root: Path) -> Iterable[Path]:
-    for entry in repo.commit().tree.traverse():
-        path = Path(entry.path) # type: ignore
-        if not path.is_dir() and _path_part_of(root, path):
-            yield path
+@lru_cache
+def _get_dirty() -> set[Path]:
+    return set(Path(entry.a_path) for entry in repo.index.diff(None) if not Path(entry.a_path).is_dir())
 
 def bump(
     major: bool = False,
     minor: bool = False,
     micro: bool = False,
     dev: bool = False,
-    commit: bool = True,
-    test_upload: bool = False,
-    upload: bool = True
+    force: bool = False
 ) -> int:
 
     import toml
 
-    # none = not (major or minor or patch or dev)
+    none = not (major or minor or micro or dev)
 
     pyproject_toml = _get_pyproject_toml()
 
@@ -119,58 +107,52 @@ def bump(
     if dev:
         v_dev = 0 if v_dev is None else v_dev + 1
 
-    # Infer whether this is a stable or nightly package
-    mode = 'stable' if v_dev is None else 'nightly'
-
     new_version = _build_version(v_maj, v_min, v_micro, v_dev)
 
-    # This lists all the files that will be added to the package
-    files = [ 'src', 'pyproject.toml', 'README.md', 'LICENSE.txt', 'Dockerfile' ]
+    dirty = _get_dirty()
 
-    files = list(Path(fname) for fname in files)
-    out_dir = project_root / 'pkg' / mode
-
-    repo = git.Repo(project_root)
-
-    # Check whether there are unstaged changes on these scanned files
-    dirty = set[Path]()
-    for entry in repo.index.diff(None):
-        path = Path(entry.a_path)
-        for other in files:
-            if _path_part_of(other, path):
-                dirty.add(path)
-
-    if commit and mode == 'stable' and repo.is_dirty():
-        print(f'Error: in order to commit the new version the repository must be clean')
+    if not force and Path('pyproject.toml') in dirty:
+        print(f"Error: action would overwrite changes in pyproject.toml. Please discard these changes before continuing or use --force.")
         return 1
-
-    if 'pyproject.toml' in dirty:
-        print(f"Error: action would overwrite changes in pyproject.toml. Please discard these changes before continuing.")
-        return 1
-
-    # Scan for files that need to be published
-    to_copy = set[Path]()
-    for path in files:
-        if path.is_dir():
-            to_copy.update(_git_list_files_in_dir(repo, path))
-        else:
-            to_copy.add(path)
-
-    if dirty:
-        print(f"Error: there are unsaved changes:")
-        for path in dirty:
-            print(f' - {path}')
-        return 1
-
-    # At this point all pre-flight checks have been performed
-
-    print(f'Now building {mode} package for {new_version}')
 
     # Overwrite pyproject.toml with the bumped version number
     pyproject_toml['project']['version'] = new_version
     with open(project_root / 'pyproject.toml', 'w') as f:
         f.write('# This file is programmatically overwritten. Do not attempt to format or add comments.\n')
         toml.dump(pyproject_toml, f)
+
+    return 0
+
+type PackageType = Literal['stable', 'nightly']
+
+def build() -> int:
+
+    version = _get_version()
+
+    # Infer whether this is a stable or nightly package
+    mode = 'stable' if version.dev is None else 'nightly'
+
+    print(f'Now building {mode} package for {version}')
+
+    # This lists all the files that will be made available for packaging
+    include = [ 'src', 'pyproject.toml', 'README.md', 'LICENSE.txt', 'Dockerfile' ]
+
+    include = list(Path(fname) for fname in include)
+    out_dir = project_root / 'pkg' / mode
+
+    # Scan for files that need to be published
+    to_copy = set[Path]()
+    for entry in repo.commit().tree.traverse():
+        child = Path(entry.path) # type: ignore
+        print(child, any(_path_same_or_part_of(root, child) for root in include))
+        if not child.is_dir() and any(_path_same_or_part_of(root, child) for root in include):
+            to_copy.add(child)
+
+    # Check whether there are unstaged changes on these scanned files
+    dirty = _get_dirty()
+    if any(path in dirty for path in to_copy):
+        print(f'Error: trying to build a package from files that have uncommitted changes')
+        return 1
 
     # Start with a fresh slate in the package directory
     # These files should be tracked by Git so it is not dangerous to remove then
@@ -184,33 +166,45 @@ def bump(
         print(f' - {path}')
         shutil.copy(path, new_path)
 
-    # Build a package that can be uploaded to PyPI
-
     try:
         from build import ProjectBuilder
     except ImportError:
         print(f"Error: could not locate the 'build' package. Ensure that it is installed with `python3 -m pip install --upgrade build`")
         return 1
 
-    dist_paths = []
     builder = ProjectBuilder(out_dir)
-    dist_paths.append(builder.build('sdist', out_dir / 'dist'))
-    dist_paths.append(builder.build('wheel', out_dir / 'dist'))
+    builder.build('sdist', out_dir / 'dist')
+    builder.build('wheel', out_dir / 'dist')
 
-    container_name = f'magelang-{mode}'
-    subprocess.run([ 'docker', 'build', '.', '-t', container_name ], cwd=out_dir, check=True)
-    subprocess.run([ 'docker', 'run', '--entrypoint', 'pytest', '-it', container_name, '--pyargs', 'magelang' ], check=True)
-    subprocess.run([ 'docker', 'run', '-it', container_name, 'help'  ], check=True)
+    return 0
 
-    if not commit:
-        return 0
+def test(mode: PackageType | None = None) -> int:
+    if mode is None:
+        import pytest
+        return pytest.main([ 'src/magelang' ])
+    else:
+        out_dir = project_root / 'pkg' / mode
+        container_name = f'magelang-{mode}'
+        subprocess.run([ 'docker', 'build', '.', '-t', container_name ], cwd=out_dir, check=True)
+        subprocess.run([ 'docker', 'run', '--entrypoint', 'pytest', '-it', container_name, '--pyargs', 'magelang' ], check=True)
+        subprocess.run([ 'docker', 'run', '-it', container_name, 'help'  ], check=True)
+    return 0
 
-    if mode == 'stable':
-        repo.index.add('pkg/stable')
-        repo.index.commit(f'Bump stable version to {new_version}')
+def commit() -> int:
+    if repo.is_dirty():
+        print(f'Error: in order to commit the new version the repository must be clean')
+        return 1
+    pyproject_toml = _get_pyproject_toml()
+    version = pyproject_toml['project']['version']
+    repo.index.add('pkg/stable')
+    repo.index.commit(f'Bump stable version to {version}')
+    return 0
 
-    if not upload:
-        return 0
+def publish(mode: PackageType = 'nightly', testing: bool = False) -> int:
+
+    out_dir = project_root / 'pkg' / mode
+
+    dist_paths = list(str(path) for path in (out_dir / 'dist').iterdir())
 
     try:
         from twine.commands.upload import upload as twine_upload
@@ -219,14 +213,10 @@ def bump(
         print(f"Error: could not locate the 'twine' package. Ensure that it is installed with `python3 -m pip install --upgrade twine`")
         return 1
 
-    repo_name = 'testpypi' if test_upload else 'pypi'
+    repo_name = 'testpypi' if testing else 'pypi'
     twine_upload(Settings(repository_name=repo_name), dist_paths)
 
     return 0
-
-def test() -> int:
-    import pytest
-    return pytest.main([ 'src/magelang' ])
 
 if __name__ == '__main__':
     from magelang.cli import run
