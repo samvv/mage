fc5719159cf20dc018d59f56414c376f9409e8c5
Sam Vervaeck <samvv@pm.me>
2025-01-26 18:06:53 +0100

    Make `mage_inline` pass make use of `lookup_ref`

--- src/magelang/passes/mage_inline.py
+++ src/magelang/passes/mage_inline.py
@@ -6,12 +6,11 @@ def mage_inline(grammar: MageGrammar) -> MageGrammar:
 
     def rewrite_expr(expr: MageExpr) -> MageExpr:
         if isinstance(expr, MageRefExpr):
-            rule = nonnull(expr.symbol).definition
+            rule = nonnull(lookup_ref(expr))
             assert(isinstance(rule, MageRule))
             if rule is None or rule.is_public or rule.is_extern or rule.expr is None:
                 return expr
             new_expr = rule.expr.derive(label=expr.label or rule.name)
-            transfer_symbols(rule.expr, new_expr)
             return rewrite_expr(new_expr)
         return rewrite_each_child_expr(expr, rewrite_expr)
 
@@ -24,18 +23,12 @@ def mage_inline(grammar: MageGrammar) -> MageGrammar:
                 if new_expr is element.expr:
                     return element
                 new_element = element.derive(expr=new_expr)
-                transfer_symbols(element, new_element)
                 return new_element
             return element
         elif isinstance(element, MageModule):
             return rewrite_module(element, rewrite_element)
         else:
             assert_never(element)
-
-    def transfer_symbols(old: MageSyntax, new: MageSyntax) -> None:
-        if (isinstance(old, MageRefExpr) and isinstance(new, MageRefExpr)) \
-                or (isinstance(old, MageRule) and isinstance(new, MageRule)):
-            new.symbol = old.symbol
 
     return rewrite_module(grammar, rewrite_element)
 
