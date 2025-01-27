4c2bfab5e470cddff32d955901ffab96400eb379
Sam Vervaeck <samvv@pm.me>
2025-01-27 23:21:57 +0100

    Format call to `generate_files` in fuzz.py

--- src/magelang/fuzz.py
+++ src/magelang/fuzz.py
@@ -16,7 +16,18 @@ from magelang.util import unreachable
 
 def load_parser(grammar: MageGrammar, native: bool = False, enable_tokens: bool = True) -> ModuleType:
     if native:
-        source = cast(str, magelang.generate_files(grammar, lang='python', enable_cst=True, enable_parser=True, emit_single_file=True, silent=True, enable_ast=False, enable_emitter=False))
+        # TODO conditionally enable the lexer and fuzz that as well
+        source = cast(str, magelang.generate_files(
+            grammar,
+            lang='python',
+            enable_cst=True,
+            enable_parser=True,
+            emit_single_file=True,
+            silent=True,
+            enable_ast=False,
+            enable_emitter=False,
+            enable_lexer=False
+        ))
         spec = importlib.util.spec_from_loader('magelang.fuzzparser', loader=None)
         assert(spec is not None)
         module = importlib.util.module_from_spec(spec)
