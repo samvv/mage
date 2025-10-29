868d3760fff73dc3234c0df308a5e66d88b95e85
Sam Vervaeck <samvv@pm.me>
2025-10-29 16:23:59 +0100

    Fix parents not being set on extracted literals

--- src/magelang/passes/mage_extract_literals.py
+++ src/magelang/passes/mage_extract_literals.py
@@ -65,7 +65,7 @@ def mage_extract_literals(
                     name = generate_token_name()
                 if expr.text not in literal_to_name:
                     literal_to_name[expr.text] = name
-                return MageRefExpr(name)
+                return MageRefExpr(name, parent=expr.parent)
             return rewrite_each_child_expr(expr, rewrite_expr)
 
         for element in node.elements:
