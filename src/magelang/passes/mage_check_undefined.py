410b7434866b8131c6db534c8acfedc9e66aa276
Sam Vervaeck <samvv@pm.me>
2025-10-30 20:47:57 +0100

    Let 'mage_check_undefined' pass make use of diagnostics

--- src/magelang/passes/mage_check_undefined.py
+++ src/magelang/passes/mage_check_undefined.py
@@ -1,15 +1,18 @@
 
-from magelang.logging import error
 from magelang.lang.mage.ast import MageGrammar, MageRefExpr, MageExpr, MageRule, for_each_direct_child_expr, for_each_rule, lookup_ref
 from magelang.manager import declare_pass
+from magelang.runtime.diagnostics import Diagnostic, Diagnostics, Severity
 
 @declare_pass()
-def mage_check_undefined(grammar: MageGrammar) -> MageGrammar:
+def mage_check_undefined(
+    grammar: MageGrammar,
+    diagnostics: Diagnostics
+) -> MageGrammar:
 
     def visit_expr(expr: MageExpr) -> None:
         if isinstance(expr, MageRefExpr):
             if lookup_ref(expr) is None:
-                error(f"undefined rule referenced: {expr.name}")
+                diagnostics.add(Diagnostic(Severity.error, f"'{expr.name}' is not a known rule at this scope", grammar.file, expr.span))
             return
         for_each_direct_child_expr(expr, visit_expr)
 
