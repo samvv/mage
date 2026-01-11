d031ab48ac1ffdbba43b55aa9d15741063ef1e01
Sam Vervaeck <samvv@pm.me>
2026-01-11 12:39:05 +0100

    Update BaseNode.__init__ logic
     - Allow BaseToken to be default-constructed
     - Fix mapping coercion over list/tuple/... not working
     - Enable repeating default-construcible collections like list
     - Add support for positional arguments in __init__.py

--- src/magelang/runtime/node.py
+++ src/magelang/runtime/node.py
@@ -17,13 +17,17 @@ __all__ = [
 type Type = Any
 
 class CoerceError(RuntimeError):
-    pass
+
+    def __init__(self, value: Any, ty: Type) -> None:
+        super().__init__(f"failed to coerce {value} to {ty}")
 
 def _get[T](seq: Sequence[T], index: int, default: T) -> T:
     return default if index >= len(seq) or index < 0 else seq[index]
 
 def is_default_constructible(ty: Type) -> bool:
     cls = typing.get_origin(ty) or ty
+    if issubclass(cls, BaseToken):
+        return not typing.get_type_hints(cls)
     return cls in [ list, Punctuated ]
 
 def construct_default(ty: Type) -> Any:
@@ -49,14 +53,14 @@ def coerce(value: Any, ty: Type, forbid_default: bool = False) -> Any:
         return value
 
     # try all element types in a union type
-    origin = typing.get_origin(ty)
+    origin = typing.get_origin(ty) or ty
     if origin is typing.Union or origin is UnionType:
         for arg in typing.get_args(ty):
             try:
                 return coerce(value, arg, True)
             except CoerceError:
                 pass
-        raise CoerceError()
+        raise CoerceError(value, ty)
 
     # if `value` is None and the type is not explicitly None, we attempt to
     # construct a default value
@@ -64,21 +68,38 @@ def coerce(value: Any, ty: Type, forbid_default: bool = False) -> Any:
         return construct_default(ty)
 
     # map coercion over a tuple type
-    if type(value) is tuple and ty is tuple:
+    if type(value) is tuple and origin is tuple:
         args = typing.get_args(ty)
-        required = [ (i, arg) for i, arg in enumerate(args) if not is_optional(arg) and not is_default_constructible(arg) ]
         new_elements = []
-        if len(required) == 1:
-            k, main_ty = required[0]
-            for i, arg in args:
-                new_elements.append(coerce(value, main_ty, True) if i == k else construct_default(arg))
-        else:
-            for i, element in enumerate(value):
-                new_elements.append(coerce(element, args[i], False))
+        for i, element in enumerate(value):
+            new_elements.append(coerce(element, args[i], False))
         return tuple(new_elements)
 
+    if origin is tuple:
+        args = typing.get_args(ty)
+        required = [ (i, arg) for i, arg in enumerate(args) if not is_optional(arg) and not is_default_constructible(arg) ]
+        if len(required) == 1:
+            k, main_ty = required[0]
+            new_elements = []
+            for i, arg in enumerate(args):
+                new_elements.append(coerce(value, main_ty, True) if i == k else construct_default(arg))
+            return tuple(new_elements)
+
+    # repeat a punctuated type
+    if isinstance(value, int) and origin is Punctuated:
+        p = Punctuated()
+        args = typing.get_args(ty)
+        el_ty = _get(args, 0, Any)
+        sep_ty = _get(args, 1, Any)
+        for _ in range(value-1):
+            p.append(
+                construct_default(el_ty),
+                construct_default(sep_ty)
+            )
+        p.append_final(construct_default(el_ty))
+
     # map coercion over a punctuated type
-    if type(value) is Punctuated and ty is Punctuated:
+    if type(value) is Punctuated and origin is Punctuated:
         args = typing.get_args(ty)
         el_ty = _get(args, 0, Any)
         sep_ty = _get(args, 1, Any)
@@ -92,25 +113,39 @@ def coerce(value: Any, ty: Type, forbid_default: bool = False) -> Any:
                 out.append_final(new_element)
         return out
 
+    # repeat a list type
+    if isinstance(value, int) and origin is list:
+        args = typing.get_args(ty)
+        el_ty = _get(args, 0, Any)
+        return [ construct_default(el_ty) for _ in range(value) ]
+
     # map coercion over a list
-    if type(value) is list and ty is list:
+    if type(value) is list and origin is list:
         args = typing.get_args(ty)
         el_ty = _get(args, 0, Any)
         return [ coerce(element, el_ty, True) for element in value ]
 
-    raise CoerceError()
+    raise CoerceError(value, ty)
 
 class BaseSyntax:
 
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         self._parent = None
         hints = typing.get_type_hints(self.__class__)
-        for name, ty in hints.items():
-            setattr(self, name, coerce(kwargs.get(name), ty))
+        to_visit = list(hints)
+        for name, value in kwargs.items():
+            setattr(self, name, coerce(value, hints[name]))
+            to_visit.remove(name)
+        for arg in args:
+            name = to_visit[0]
+            setattr(self, name, coerce(arg, hints[name]))
+            to_visit.remove(name)
+        for name in to_visit:
+            setattr(self, name, coerce(None, hints[name]))
 
     def derive(self, **kwargs) -> Self:
         hints = typing.get_type_hints(self.__class__)
-        for name  in hints:
+        for name in hints:
             if name not in kwargs:
                 kwargs[name] = getattr(self, name)
         return self.__class__(**kwargs)
--- src/magelang/runtime/test_node.py
+++ src/magelang/runtime/test_node.py
@@ -1,5 +1,5 @@
 
-from .node import BaseNode
+from .node import BaseNode, BaseToken
 
 def test_construct_kwargs():
 
@@ -21,12 +21,28 @@ def test_construct_args():
     assert(f.foo == 1)
     assert(f.bar == 'blabla')
 
-def test_default_construct_list():
+def test_construct_kwarg_before_arg():
 
-    class Node(BaseNode):
-        pass
+    class Foo(BaseNode):
+        foo: int
+        bar: str
+        bax: bool
 
-    class Foo(Node):
+    f = Foo('blabla', True, foo=1)
+    assert(f.foo == 1)
+    assert(f.bar == 'blabla')
+
+def test_construct_none():
+
+    class Foo(BaseNode):
+        blabla: int | None
+
+    f = Foo()
+    assert(f.blabla is None)
+
+def test_default_construct_list_from_nothing():
+
+    class Foo(BaseNode):
         blabla: list[int]
         hello: str
 
@@ -35,3 +51,56 @@ def test_default_construct_list():
     assert(len(f.blabla) == 0)
     assert(f.hello == 'hello')
 
+def test_default_construct_list_from_none():
+
+    class Foo(BaseNode):
+        blabla: list[int]
+        hello: str
+
+    f = Foo(blabla=None, hello='hello')
+    assert(isinstance(f.blabla, list))
+    assert(len(f.blabla) == 0)
+    assert(f.hello == 'hello')
+
+def test_repeat_list():
+
+    class Foo(BaseNode):
+        l: list[list[int]]
+
+    f = Foo(5)
+    assert(isinstance(f.l, list))
+    assert(len(f.l) == 5)
+
+def test_repeat_list_named():
+
+    class Comma(BaseToken):
+        pass
+
+    class Foo(BaseNode):
+        l: list[Comma]
+
+    f = Foo(l=5)
+    assert(isinstance(f.l, list))
+    assert(len(f.l) == 5)
+    assert(isinstance(f.l[0], Comma))
+    assert(isinstance(f.l[1], Comma))
+    assert(isinstance(f.l[2], Comma))
+    assert(isinstance(f.l[3], Comma))
+    assert(isinstance(f.l[4], Comma))
+
+def test_coerce_list_elements():
+
+    class Comma(BaseToken):
+        pass
+
+    class Bar(BaseNode):
+        pass
+
+    class Foo(BaseNode):
+        l: list[tuple[Bar, Comma, Comma]]
+
+    f = Foo([ Bar(), Bar() ])
+    assert(isinstance(f.l, list))
+    assert(len(f.l) == 2)
+    assert(isinstance(f.l[0], tuple))
+    assert(isinstance(f.l[1], tuple))
